75e7a9e5b79e1066cb8dc22fb3947c735c0ba63d
Tyler Long <tyler4long@gmail.com>
2018-04-03 16:17:09 +0800

    Add test case for multipart/mixed

--- ringcentral/http/multipart_builder_test.py
+++ ringcentral/http/multipart_builder_test.py
@@ -6,10 +6,15 @@ import unittest
 from ..test import TestCase
 from .multipart_builder import MultipartBuilder
 
+class MockPlatform:
+    def create_url(self, url, add_server):
+        if add_server:
+            return 'http://example.com/' + url
+        return url
 
 class TestMultipartBuilder(TestCase):
     def test_add(self):
-        mb = MultipartBuilder(None)
+        mb = MultipartBuilder(MockPlatform())
 
         mb.set_body({'foo': 'bar'})
         mb.add(('report.csv', 'some,data,to,send'))
@@ -22,6 +27,19 @@ class TestMultipartBuilder(TestCase):
             ('json', ('request.json', '{"foo": "bar"}', 'application/json')),
             ('attachment', ('report.csv', 'some,data,to,send'))
         ])
+
+    def test_multipart_mixed(self):
+        mb = MultipartBuilder(MockPlatform())
+
+        mb.set_body({'foo': 'bar'})
+        mb.add(('report.csv', 'some,data,to,send'))
+        mb.set_multipart_mixed(True)
+
+        req = mb.request('/foo')
+
+        self.assertTrue('multipart/mixed' in req.headers['Content-Type'], True)
+        self.assertEqual(mb.body(), {'foo': 'bar'})
+        self.assertEqual(mb.contents(), [('attachment', ('report.csv', 'some,data,to,send'))])
 
 
 if __name__ == '__main__':
