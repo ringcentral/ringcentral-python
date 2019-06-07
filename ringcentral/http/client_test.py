8c0e66c0ee443d9b2c3d51ebf196ef78cf9eda5e
PacoVu <paco_vu@yahoo.com>
2019-06-06 21:02:01 -0700

    Update client_test.py
    Split test case for testing array params in query string

--- ringcentral/http/client_test.py
+++ ringcentral/http/client_test.py
@@ -14,6 +14,9 @@ class TestClient(TestCase):
         req = Client().create_request(url='http://whatever', query_params={'foo': 'bar', 'baz': 'qux'})
         self.assertEqual('http://whatever?foo=bar&baz=qux', req.url)
 
+        req = Client().create_request(url='http://whatever', query_params={'a2z':['abc','xyz']})
+        self.assertEqual('http://whatever?a2z=abc&a2z=xyz', req.url)
+
     def test_create_request_encode_body_url(self):
         r = Client().create_request(body=body, headers={'Content-Type': 'application/x-www-form-urlencoded'})
         self.assertEqual('foo=bar&baz=qux', r.data)
