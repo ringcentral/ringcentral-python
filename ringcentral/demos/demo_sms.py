67726dacbe6ab505b85e947320e9c7361cd6528e
Tyler Liu <tyler4long@gmail.com>
2025-04-15 13:32:22 -0700

    Try to print response status_code

--- ringcentral/demos/demo_sms.py
+++ ringcentral/demos/demo_sms.py
@@ -1,17 +1,35 @@
-from ringcentral import SDK
+import os
+import sys
+
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+)  # Add the project root to sys.path
 from dotenv import dotenv_values
+
+from ringcentral import SDK
 
 env = dotenv_values(".env")
 
+
 def main():
-    sdk = SDK(env['RINGCENTRAL_CLIENT_ID'], env['RINGCENTRAL_CLIENT_SECRET'], env['RINGCENTRAL_SERVER_URL'])
+    sdk = SDK(
+        env["RINGCENTRAL_CLIENT_ID"],
+        env["RINGCENTRAL_CLIENT_SECRET"],
+        env["RINGCENTRAL_SERVER_URL"],
+    )
     platform = sdk.platform()
-    platform.login(jwt = env['RINGCENTRAL_JWT_TOKEN'])
+    platform.login(jwt=env["RINGCENTRAL_JWT_TOKEN"])
 
-    params = {'from': {'phoneNumber': env['RINGCENTRAL_SENDER']},'to': [{'phoneNumber': env['RINGCENTRAL_RECEIVER']}],'text': "SMS message"}
-    response = platform.post('/restapi/v1.0/account/~/extension/~/sms', params)
+    params = {
+        "from": {"phoneNumber": env["RINGCENTRAL_SENDER"]},
+        "to": [{"phoneNumber": env["RINGCENTRAL_RECEIVER"]}],
+        "text": "SMS message",
+    }
+    response = platform.post("/restapi/v1.0/account/~/extension/~/sms", params)
 
-    print('Sent SMS: ' + response.json().uri)
+    print("Sent SMS: " + response.json().uri)
+    print(response.response().status_code)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
